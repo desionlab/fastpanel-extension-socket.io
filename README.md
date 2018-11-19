1dd05ddf94cd7d937b294b54ffbc2a77d077d162
fenixphp <fenixphp@gmail.com>
2018-11-19 14:33:41 +0300

    Init project.

---
+++ README.md
@@ -0,0 +1,12 @@
+# fastPanel WebSocket Extension.
+Extension to work with the "WebSocket" protocol.
+
+---
+
+## License
+The MIT License (MIT)
+
+---
+
+## Copyright
+(c) 2018 Desionlab
---
+++ test/index.spec.ts
@@ -0,0 +1,17 @@
+/**
+ * index.spec.ts
+ * 
+ * @author    Desionlab <fenixphp@gmail.com>
+ * @copyright 2018 Desionlab
+ * @license   MIT
+ */
+
+import { expect } from 'chai';
+
+describe("fastPanel WebSocket Extension", () => {
+
+  
+
+});
+
+/* End of file index.spec.ts */
