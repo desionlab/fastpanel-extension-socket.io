fed06b9a72007eb78120af10a56469568e994abb
fenixphp <fenixphp@gmail.com>
2018-11-21 19:14:21 +0300

    [WIP] Basic classes.

--- build/Extension.js
+++ build/Extension.js
@@ -6,7 +6,13 @@
  * @copyright 2014 - 2018 Desionlab
  * @license   MIT
  */
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const socket_io_1 = __importDefault(require("socket.io"));
+const SocketIOEmitter = require('socket.io-emitter');
+const socket_io_redis_1 = __importDefault(require("socket.io-redis"));
 const fastpanel_core_1 = require("fastpanel-core");
 /**
  * Class Extension
@@ -19,11 +25,39 @@ class Extension extends fastpanel_core_1.Extensions.ExtensionDefines {
     /**
      * Registers a service provider.
      */
-    async register() { }
+    async register() {
+        if (this.context instanceof fastpanel_core_1.Cli.Handler) {
+            /* Registration websocket emitter. */
+            this.di.set('socket', (container) => {
+                let socket = SocketIOEmitter(container.get('redis', this.config.get('Extensions/SocketIO.redis', null)));
+                return socket;
+            }, true);
+        }
+        else if (this.context instanceof fastpanel_core_1.Cluster.Handler) {
+            /* Registration websocket server. */
+            this.di.set('socket', (container) => {
+                /* Create server. */
+                let socket = socket_io_1.default(this.http, {
+                    path: '/real-time'
+                });
+                /* Set io adapter. */
+                socket.adapter(socket_io_redis_1.default({
+                    pubClient: container.get('redis', this.config.get('Extensions/SocketIO.redis', null)),
+                    subClient: container.get('redis', this.config.get('Extensions/SocketIO.redis', null))
+                }));
+                return socket;
+            }, true);
+        }
+    }
     /**
      * Startup a service provider.
      */
-    async startup() { }
+    async startup() {
+        if (this.di.has('socket')) {
+            /* Fire http startup event. */
+            this.events.emit('socket:startup', this.socket);
+        }
+    }
 }
 exports.Extension = Extension;
 /* End of file Extension.ts */ 
--- src/Extension.ts
+++ src/Extension.ts
@@ -6,7 +6,10 @@
  * @license   MIT
  */
 
-import { Di, Extensions } from 'fastpanel-core';
+import SocketIO from 'socket.io';
+const SocketIOEmitter = require('socket.io-emitter');
+import SocketIORedisAdapter from 'socket.io-redis';
+import { Cli, Cluster, Di, Extensions } from 'fastpanel-core';
 
 /**
  * Class Extension
@@ -20,12 +23,43 @@ export class Extension extends Extensions.ExtensionDefines {
   /**
    * Registers a service provider.
    */
-  async register () : Promise<any> {}
+  async register () : Promise<any> {
+    if (this.context instanceof Cli.Handler) {
+      /* Registration websocket emitter. */
+      this.di.set('socket', (container: Di.Container) => {
+        let socket = SocketIOEmitter(
+          container.get('redis', this.config.get('Extensions/SocketIO.redis', null))
+        );
+        return socket;
+      }, true);
+    } else if (this.context instanceof Cluster.Handler) {
+      /* Registration websocket server. */
+      this.di.set('socket', (container: Di.Container) => {
+        /* Create server. */
+        let socket = SocketIO(this.http, {
+          path: '/real-time'
+        });
+
+        /* Set io adapter. */
+        socket.adapter(SocketIORedisAdapter({
+          pubClient: container.get('redis', this.config.get('Extensions/SocketIO.redis', null)),
+          subClient: container.get('redis', this.config.get('Extensions/SocketIO.redis', null))
+        }));
+
+        return socket;
+      }, true);
+    }
+  }
   
   /**
    * Startup a service provider.
    */
-  async startup () : Promise<any> {}
+  async startup () : Promise<any> {
+    if (this.di.has('socket')) {
+      /* Fire http startup event. */
+      this.events.emit('socket:startup', this.socket);
+    }
+  }
 
 }
 
